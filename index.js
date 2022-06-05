f155ea6e5e1ceb08f4f754386da8e0794afd0a84
le-dat <lqdat87@gmail.com>
2022-06-05 11:13:53 +0700

    upload source backend

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+app.post("/v1/update", (req, res) => {
+  setTimeout(() => {
+    res.status(200).json(req.body);
+  }, [2000]);
+});
+
+app.listen(5000, () => {
+  console.log("Server is rendering in http://localhost:5000");
+});
